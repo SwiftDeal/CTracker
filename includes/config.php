4520aee1e262aef8662458e7ea90614adf7abb3b
Faizan Ayubi <indianayubi@gmail.com>
2016-02-12 23:00:02 +0530

    Warnings Supressed

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define(maxmind_db_path,'/var/www/powerfeeds/includes/GeoLite2-Country.mmdb');
+define('maxmind_db_path','/var/www/powerfeeds/includes/GeoLite2-Country.mmdb');
 define('CDN', "http://$_SERVER[HTTP_HOST]/images/");
 define('SITE', "http://$_SERVER[HTTP_HOST]/");
 define('DIMENSION', '560x292');
